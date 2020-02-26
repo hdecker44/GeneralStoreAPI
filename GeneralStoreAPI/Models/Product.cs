ff22b82d404d9ed5acddd1b87f383520b277589e
Henry Decker <henryjdecker@gmail.com>
2020-02-26 14:58:10 -0500

    Add project files.

---
+++ GeneralStoreAPI/Models/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GeneralStoreAPI.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+
+        public string Name { get; set; }
+
+        [Required]
+        [Range(0, double.MaxValue)]
+        [DataType(DataType.Currency)]
+        public decimal Price { get; set; }
+
+        [Required]
+        public string Brand { get; set; }
+
+        [Required]
+        public int Quantity { get; set; }
+    }
+}
---
+++ GeneralStoreAPI/Models/Transaction.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace GeneralStoreAPI.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(Product))]
+        public int ProductId { get; set; }
+        public virtual Product Product { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(Customer))]
+        public int CustomerId { get; set; }
+        public virtual Customer Customer { get; set; }
+
+
+        public DateTime DateOfTransaction { get; set; }
+
+        public decimal TotalCost
+        {
+            get
+            {
+                return Product.Price * ProductCount;
+            }
+        }
+
+        public int ProductCount { get; set; }
+    }
+}
